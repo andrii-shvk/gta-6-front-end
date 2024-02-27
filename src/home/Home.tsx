bb9cdca5c805b38eb1ed68f3aee273dc6e7aba4b
Andrii Shevchuk <supamercron@gmail.com>
2024-02-27 15:33:10 -0500

    styling form request with Tailwind

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -2,13 +2,21 @@ import styles from './Home.module.css'
 
 function Home() {
 
+  const isSuccess = false;
+
   return (
       <div className={styles.wrapper}>
         <form>
-          <h1>GTA 6 - Leave a request</h1>
-          <input type="name" placeholder='Enter Name:' />
-          <input type="email" placeholder='Enter Email:' />
-          <button>I Want A GTA!</button>
+            {isSuccess ? (
+                <div className={styles.success}>Form Submitted!</div>
+            ) : (
+                <>
+                    <h1>GTA 6 - Leave a request</h1>
+                    <input type="name" placeholder='Enter Name:' />
+                    <input type="email" placeholder='Enter Email:' />
+                    <button>I Want A GTA!</button>
+                </>
+            )}
         </form>
       </div>
   )
