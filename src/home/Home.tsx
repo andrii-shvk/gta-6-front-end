7c19d1793356fc35e7cbbfa6c2f929a612b1b4c8
Andrii Shevchuk <supamercron@gmail.com>
2024-02-27 15:43:43 -0500

    add react-hook-form

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,19 +1,29 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
 import styles from './Home.module.css'
 
-function Home() {
+const isSuccess = false;
+interface IFormState {
+    name: string,
+    email: string
+}
 
-  const isSuccess = false;
+function Home() {
+    const {register, handleSubmit} = useForm<IFormState>();
+
+    const onSubmit: SubmitHandler<IFormState> = (data) => {
+        console.log(data);
+    }
 
   return (
       <div className={styles.wrapper}>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             {isSuccess ? (
                 <div className={styles.success}>Form Submitted!</div>
             ) : (
                 <>
                     <h1>GTA 6 - Leave a request</h1>
-                    <input type="name" placeholder='Enter Name:' />
-                    <input type="email" placeholder='Enter Email:' />
+                    <input type="name" placeholder='Enter Name:' {...register('name')}/>
+                    <input type="email" placeholder='Enter Email:' {...register('email')}/>
                     <button>I Want A GTA!</button>
                 </>
             )}
