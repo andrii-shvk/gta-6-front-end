8c3882ba5d1e9ba0f11379bae079c4c6cb5dd190
Andrii Shevchuk <supamercron@gmail.com>
2024-02-27 22:00:07 -0500

    add fetch Submit, create back-end

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,17 +1,35 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
 import styles from './Home.module.css'
+import { useState } from 'react';
 
-const isSuccess = false;
 interface IFormState {
     name: string,
     email: string
 }
 
 function Home() {
-    const {register, handleSubmit} = useForm<IFormState>();
+    const {register, handleSubmit, reset} = useForm<IFormState>();
+    const [isSuccess, setIsSuccess] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const onSubmit: SubmitHandler<IFormState> = (data) => {
-        console.log(data);
+        setIsLoading(true)
+        fetch('http://localhost:5000/api', {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(response => 
+            response.json()
+        ).then(data => {
+            if(!data) return
+
+            setIsSuccess(true);
+            reset()
+        }).finally(() => {
+            setIsLoading(false)
+        })
     }
 
   return (
@@ -24,7 +42,7 @@ function Home() {
                     <h1>GTA 6 - Leave a request</h1>
                     <input type="name" placeholder='Enter Name:' {...register('name')}/>
                     <input type="email" placeholder='Enter Email:' {...register('email')}/>
-                    <button>I Want A GTA!</button>
+                    <button disabled={isLoading}>{isLoading ? 'Loading...' : 'I Want A GTA!'}</button>
                 </>
             )}
         </form>
